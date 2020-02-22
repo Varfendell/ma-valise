f7914aa8cafd4d510a7f35ddf0b62ad764463b17
FCA <cardot.florent@gmail.com>
2020-02-22 16:44:47 +0100

    bork

--- src/Form/Crud/UserType.php
+++ src/Form/Crud/UserType.php
@@ -5,6 +5,9 @@ namespace App\Form\Crud;
 use App\Entity\User;
 use Symfony\Component\Form\AbstractType;
 use Symfony\Component\Form\Extension\Core\Type\DateType;
+use Symfony\Component\Form\Extension\Core\Type\EmailType;
+use Symfony\Component\Form\Extension\Core\Type\FileType;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
 
@@ -13,19 +16,19 @@ class UserType extends AbstractType
     public function buildForm(FormBuilderInterface $builder, array $options)
     {
         $builder
-            ->add('firstName')
-            ->add('lastName')
-            ->add('email')
-            ->add('phone')
-            ->add('birthdate', DateType::class, [
-                'widget' => 'single_text',
-                'html5' => false,
+            ->add('firstName', TextType::class)
+            ->add('lastName', TextType::class)
+            ->add('email', EmailType::class)
+            ->add('phone', TextType::class, [
+                'required' => false,
             ])
             ->add('password')
-            ->add('salt')
-            ->add('city')
-            ->add('picture')
-        ;
+            ->add('city', TextType::class, [
+                'required' => false,
+            ])
+            ->add('picture', FileType::class, [
+                'required' => false,
+            ]);
     }
 
     public function configureOptions(OptionsResolver $resolver)
