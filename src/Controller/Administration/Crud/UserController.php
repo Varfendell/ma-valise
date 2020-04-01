1f43fef93173571d1f9c42b6f0fca93729b723eb
JeremyM <jeremy.marques@hotmail.fr>
2020-04-01 18:11:01 +0200

    remove  defaults={"id"=11}

--- src/Controller/Administration/Crud/UserController.php
+++ src/Controller/Administration/Crud/UserController.php
@@ -94,7 +94,7 @@ class UserController extends DefaultController
 	}
 
 	/**
-	 * @Route("/{id}/change-password", name="user_change_password", methods={"DELETE"}, defaults={"id"=11})
+	 * @Route("/{id}/change-password", name="user_change_password", methods={"DELETE"})
      * @param Request $request
      * @param User $user
 	 * @return Response
