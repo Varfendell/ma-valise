dad371a9c27dbc1d55189641402b371ca3a0b732
JeremyM <jeremy.marques@hotmail.fr>
2020-04-01 17:05:29 +0200

    add requirements={"name"=".+"}

--- src/Controller/Administration/Crud/UserController.php
+++ src/Controller/Administration/Crud/UserController.php
@@ -94,7 +94,7 @@ class UserController extends DefaultController
 	}
 
 	/**
-	 * @Route("/{id}/change-password", name="user_change_password", methods={"DELETE"})
+	 * @Route("/{id}/change-password", name="user_change_password", requirements={"name"=".+"}, methods={"DELETE"})
 	 * @param Request $request
 	 * @param User $user
 	 * @return Response
