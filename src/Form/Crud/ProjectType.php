d0e3dd91ce989c1e80612c39f81c9b9675b03f80
JeremyM <jeremy.marques@hotmail.fr>
2020-04-10 13:46:27 +0200

    run php bin/console make:entity for desires

--- src/Form/Crud/ProjectType.php
+++ src/Form/Crud/ProjectType.php
@@ -17,7 +17,7 @@ class ProjectType extends AbstractType
 {
 	public function buildForm(FormBuilderInterface $builder, array $options)
 	{
-		$builder->add('name', TextType::class, ['label' => 'Quel est le nom de ton projet?'])->add('desires', ChoiceType::class, ['required' => false, 'choices' => [
+		$builder->add('name', TextType::class)->add('desires', ChoiceType::class, ['required' => false, 'choices' => [
 		    '#Aventure' => false,
             '#Rando' => false,
             '#Bien-être' => false,
